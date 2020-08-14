ffa7bce1c25bad1cd3d2d3990c484a4a894107f3
thelittlecitizen7 <thelittlecitizen7@gmail.com>
2020-08-14 09:33:01 +0300

    finish timer logic

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeMiliseconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time ff = new Time(new TimeSpan(12,3,44,44));
+            ff.Print();
+        }
+    }
+}
